a3daa56bf7e5e8542fa7dd4f682848c47acc1671
VinitGrover <vinitgrover255@gmail.com>
2022-06-19 12:23:15 +0530

    Modified Special character password

--- src/com/bridgelabz/RegexPasswordSpecialCharacter.java
+++ src/com/bridgelabz/RegexPasswordSpecialCharacter.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class RegexPasswordSpecialCharacter {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*_-]).{8,}$");
 
         System.out.println("Enter minimum 8 digit Password containing at least one uppercase and one numeric character");
         Scanner userInput = new Scanner(System.in);
