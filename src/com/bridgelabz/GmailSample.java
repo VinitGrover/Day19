da4f39b854260a9b6b957706f5e20f454481ac8f
VinitGrover <vinitgrover255@gmail.com>
2022-06-19 12:46:48 +0530

    Added uc8_GmailSample file

--- src/com/bridgelabz/GmailSample.java
+++ src/com/bridgelabz/GmailSample.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexPasswordSpecialCharacter {
+public class GmailSample {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*_-]).{8,}$");
+        Pattern pattern = Pattern.compile("^(abc)[.+-]?[0-9]{0,3}(@)[a-z0-9]{1,}.[a-z]{1,}.?[a-z]{1,},?$");
 
-        System.out.println("Enter minimum 8 digit Password containing at least one uppercase, one numeric character, one specail character");
+        System.out.println("Enter Gmail to verify");
         Scanner userInput = new Scanner(System.in);
         String password = userInput.nextLine();
 
         Matcher matcher = pattern.matcher(password);
 
         if (matcher.matches() == false) {
-            System.out.println("Invalid Password");
+            System.out.println("Invalid Gmail");
         } else {
-            System.out.println("Valid Password");
+            System.out.println("Valid Gmail");
         }
     }
 }
